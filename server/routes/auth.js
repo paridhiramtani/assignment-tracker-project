c56713f9df1f8cbbc90393e910a2ca2039385102
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-07 20:28:29 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,10 @@
-// server/routes/auth.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Public routes
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-
-// Protected routes
 router.get('/me', authMiddleware, authController.getMe);
 
 module.exports = router;
