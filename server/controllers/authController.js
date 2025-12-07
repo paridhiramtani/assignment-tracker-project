5b4cb72f8c28510797b7f19eae9bdccfb313219f
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-07 20:28:09 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,3 @@
-// server/controllers/authController.js
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
@@ -110,15 +109,3 @@ exports.getMe = async (req, res) => {
     role: req.user.role
   });
 };
-
-// server/routes/auth.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.get('/me', authMiddleware, authController.getMe);
-
-module.exports = router;
