091ae0518023212b71f0c4787b451d3d25495ad7
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-09 15:22:31 +0530

    Update Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,25 +1,8 @@
 const mongoose = require('mongoose');
-
 const messageSchema = new mongoose.Schema({
-  course: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course',
-    required: true
-  },
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  content: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true, trim: true },
+  createdAt: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('Message', messageSchema);
